5f23ef5ffab1d21f4243e16e12dca08a8a5e028d
nariman <nariman.tatari@gmail.com>
2023-11-20 15:09:42 +0330

    fix home page

--- components/templates/HomePage.js
+++ components/templates/HomePage.js
@@ -1,10 +1,10 @@
+import Attributes from '../modules/Attributes';
 import Banner from '../modules/Banner';
 import Companies from '../modules/Companies';
 import Definition from '../modules/Definition';
 import Guide from '../modules/Guide';
 import Instruction from '../modules/Instruction';
 import Restrictions from '../modules/Restrictions';
-import Attributes from '../modules/attributes';
 import styles from './HomePage.module.css'
 
 const HomePage = () => {
