df617e272eaa6773542a1d6635264d2185c7b5ab
nariman <nariman.tatari@gmail.com>
2023-11-20 11:06:13 +0330

    fix api server

--- pages/menu/[id].js
+++ pages/menu/[id].js
@@ -18,7 +18,7 @@ const Details = ({data}) => {
 export default Details;
 export async function getStaticPaths()
 {
-    const res=await fetch('http://localhost:3001/data');
+    const res=await fetch('https://fooddbapi.vercel.app/data');
     const json=await res.json();
     const data=json.slice(0,10); 
     const paths=data.map(food=>({params:{id:food.id.toString()}}))
@@ -31,7 +31,7 @@ export async function getStaticPaths()
 
 export async function getStaticProps(context){
     const {params}=context;
-    const res=await fetch(`http://localhost:3001/data/${params.id}`);
+    const res=await fetch(`https://fooddbapi.vercel.app/data/${params.id}`);
     const data=await res.json();
     if(!data.id){
         return {
